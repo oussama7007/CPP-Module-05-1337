4510df9b805ab785333a15f931312e7b97414f70
aitsim <oussamatachakourt@gmail.com>
2026-02-11 20:58:10 +0100

    Auto commit: 2026-02-11 20:58:10

--- ex00/Bureaucrat.cpp
+++ ex00/Bureaucrat.cpp
@@ -8,16 +8,16 @@ Bureaucrat::Bureaucrat(const std::string &str): name(str) {}
 
 Bureaucrat::Bureaucrat() : name("default") {}
 
-Bureaucrat::Bureaucrat(const Bureaucrat &other)
+Bureaucrat::Bureaucrat(const Bureaucrat &other) : name(other.name) , grade(other.grade) {}
+
+Bureaucrat & Bureaucrat::operator=(const Bureaucrat &other)
 {
-    this->grade = other.grade;
-    this->name = other.name;
+    if(this != &other )
+    {
+        this->grade = other.grade;
+    }
+    return *this;
 }
 
-Bureaucrat & operator=(const Bureaucrat &other)
-{
-    if(this !=&other )
-    {
-        
-    }
-}
+Bureaucrat::~Bureaucrat() {}
+
