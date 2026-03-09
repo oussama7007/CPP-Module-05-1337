3f0fb000599a2148cc9967a0b57704a95adcfb75
aitsim <oussamatachakourt@gmail.com>
2026-03-09 10:12:31 +0000

    d

--- ex02/ShrubberyCreationForm.h
+++ ex02/ShrubberyCreationForm.h
@@ -17,7 +17,7 @@ public:
     ShrubberyCreationForm(const ShrubberyCreationForm &other);
     ShrubberyCreationForm &operator=(const ShrubberyCreationForm &other);
     ~ShrubberyCreationForm();
-
+ 
    
     void execute(Bureaucrat const & executor) const;
 };
