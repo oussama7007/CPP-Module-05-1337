7f56a5c28fca9ae552d1ef6e83a2141ee6fe7454
aitsim <oussamatachakourt@gmail.com>
2026-03-14 15:00:01 +0000

    Auto commit: 2026-03-14 15:00:01

--- test.cpp
+++ test.cpp
@@ -38,6 +38,116 @@ std::ostream& operator<<(std::ostream &out , const Bureaucrat& obj);
 
 
 
+class Bureaucrat;
+
+class Form
+{
+    private:
+        const std::string name;
+        bool sign;
+        const int grade_sign;
+        const int grade_execute;
+
+    public:
+        Form();
+        ~Form();
+        Form(const std::string str, bool sign, const int grade_sign, const int grade_execute);
+        Form & operator=(const Form &other);
+        Form(const Form &other);
+
+        class GradeTooHighException : public std::exception
+        {
+            public:
+                const char *what() const throw();
+        };
+        class GradeTooLowException : public std::exception
+        {
+            public:
+                const char *what() const throw();
+        };
+        
+        std::string     getName() const;
+        int             getGrade_sign() const;
+        int             getGrade_execute() const;
+        bool            getSign() const;
+
+        void            beSigned(const Bureaucrat &obj);
+};
+std::ostream    &operator<<(std::ostream &out,const Form &obj);
+
+
+Form::Form() : name("default"), sign(false), grade_sign(1),  grade_execute(1){}
+
+Form::Form(const std::string str,
+    bool sign, const int grade_sign, const int grade_execute): name(str), sign(sign), 
+    grade_sign(grade_sign), grade_execute(grade_execute)
+{
+    if(grade_execute < 1 || grade_sign < 1 )
+        throw Form::GradeTooHighException();
+    else if(grade_sign > 150 || grade_execute > 150)
+        throw Form::GradeTooLowException();
+}
+
+Form::Form(const Form &other) : name(other.name), sign(other.sign), grade_sign(other.grade_sign),
+                grade_execute(other.grade_execute)
+{}
+
+
+Form & Form::operator=(const Form &other)
+{
+    if(this != &other)
+    {
+        this->sign = other.sign;
+    }
+    return *this;
+}
+
+Form::~Form() {}
+
+const char *Form::GradeTooHighException::what() const throw()
+{
+    return " Form: Grade Too High Exception ";
+}
+
+
+
+const char *Form::GradeTooLowException::what() const throw()
+{
+    return " Form: Grade Too Low Exception ";
+}
+
+
+
+std::string     Form::getName() const
+{
+    return this->name;
+}
+
+
+
+int      Form::getGrade_execute() const 
+{
+        return this->grade_execute;
+}
+
+
+
+int     Form::getGrade_sign() const 
+{
+    return this->grade_sign;
+}
+
+
+
+bool    Form::getSign() const
+{
+        return this->sign;
+}
+
+void    bureaucrat()
+
+
+
 
 
 
