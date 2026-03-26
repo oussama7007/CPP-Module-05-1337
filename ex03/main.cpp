936dcb8b975d390783aa10adcb07670eb635f0ca
Oussama Ait Si Hamou <oait-si-@e1r4p9.1337.ma>
2026-03-26 13:56:02 +0100

    Auto update Thu Mar 26 01:56:02 PM +01 2026

---
+++ ex03/main.cpp
@@ -0,0 +1,35 @@
+
+
+#include "AForm.h"
+#include "Bureaucrat.h" 
+#include "PresidentialPardonForm.h"
+#include "RobotomyRequestForm.h"
+#include "ShrubberyCreationForm.h"
+#include "Intern.h"
+
+
+
+int main()
+{
+
+    // Test 1: valid form creation
+    Intern intern;
+    AForm *form = intern.makeForm("RobotomyRequestForm", "Bender");
+    delete form;
+    std::cout << " ---------------------------------------\n";
+    
+
+    // Test 2: invalid form name
+    AForm *form2 = intern.makeForm("unknownForm", "Bender");
+    (void)form2;
+    
+
+    // Test 3: create, sign and execute
+    std::cout << " ---------------------------------------\n";
+    Bureaucrat b("Alice", 1);
+    AForm *form3 = intern.makeForm("PresidentialPardonForm", "Bob");
+    b.signForm(*form3);
+    b.executeForm(*form3);
+    delete form3;
+
+}
