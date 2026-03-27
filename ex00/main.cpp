a77f4cdce7d59f95f466e6c220d635a12c121dbe
Oussama Ait Si Hamou <oait-si-@e2r5p7.1337.ma>
2026-03-27 05:33:14 +0100

    Auto update 2026-03-27 05:33:14

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -3,44 +3,46 @@
 
 int main()
 { 
-    try {
-        Bureaucrat b1("Frank", 12);
-        Bureaucrat b2(b1);
-        std::cout << b2;
-    }
-    catch (std::exception &e) {
-        std::cout << e.what() << std::endl;
-    }
-    std::cout <<  "-----------------------------------" << std::endl;
-    try 
-    {
-        Bureaucrat obj( "oussama", 150);
-        for(int i = 0; i <= 16; i++)
-            obj.increment();
-        std::cout << obj;
-    }
-    catch(std::exception &e)
-    {
-        std::cout << e.what() << std::endl;
-    }
-    std::cout <<  "-----------------------------------" << std::endl;
-    try {
-        Bureaucrat b("Bob", 0);
-        std::cout << b;
-    }
-    catch (std::exception &e) {
-        std::cout << e.what() << std::endl;
-    }
-    std::cout <<  "-----------------------------------" << std::endl;
-    try {
-        Bureaucrat b("Charlie", 150);
-        b.decrement();
-        Bureaucrat b1(b);
-        std::cout << b1;
-    }
-    catch (std::exception &e) {
-        std::cout << e.what() << std::endl;
-    }
+    // try {
+    //     Bureaucrat b1("Frank", 12);
+    //     Bureaucrat b2(b1);
+    //     std::cout << b2;
+    // }
+    // catch (std::exception &e) {
+    //     std::cout << e.what() << std::endl;
+    // }
+    // std::cout <<  "-----------------------------------" << std::endl;
+    // try 
+    // {
+    //     Bureaucrat obj( "oussama", 150);
+    //     for(int i = 0; i <= 16; i++)
+    //         obj.increment();
+    //     std::cout << obj;
+    // }
+    // catch(std::exception &e)
+    // {
+    //     std::cout << e.what() << std::endl;
+    // }
+    // std::cout <<  "-----------------------------------" << std::endl;
+    // try {
+    //     Bureaucrat b("Bob", 0);
+    //     std::cout << b;
+    // }
+    // catch (std::exception &e) {
+    //     std::cout << e.what() << std::endl;
+    // }
+    // std::cout <<  "-----------------------------------" << std::endl;
+    // try {
+    //     Bureaucrat b("Charlie", 150);
+    //     b.decrement();
+    //     Bureaucrat b1(b);
+    //     std::cout << b1;
+    // }
+    // catch (std::exception &e) {
+    //     std::cout << e.what() << std::endl;
+    // }
+
+        Bureaucrat b("oussama", 200);
 
 
 }
