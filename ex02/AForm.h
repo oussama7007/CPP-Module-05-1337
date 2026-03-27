09817324c559cd4aec17eda3b73768894dc89638
Oussama Ait Si Hamou <oait-si-@e2r5p7.1337.ma>
2026-03-27 02:21:50 +0100

    Auto update 2026-03-27 02:21:50

--- ex02/AForm.h
+++ ex02/AForm.h
@@ -27,7 +27,7 @@ class AForm
         class GradeTooHighException : public std::exception
         {
             public:
-                const char *what() const throw();
+                const char *what() const throw(); 
         };
         class GradeTooLowException : public std::exception
         {
